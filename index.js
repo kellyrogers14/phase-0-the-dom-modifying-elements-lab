fc80a474e07ba866fcb8c1076af8be1242c1dd97
Kelly Rogers <kellyerogers92@gmail.com>
2023-12-04 12:42:40 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Kelly Rogers is the champion"
+
+// the 'newHeader' variable that points to the <h1> node has an id of 'victory':
